cae14771f1e5538cb720b86aa77f50d4de6d8a72
fredericktownes <fredericktownes@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-08-01 13:06:55 +0000

    git-svn-id: https://plugins.svn.wordpress.org/w3-total-cache/tags/0.5@141734 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- w3-total-cache.php
+++ w3-total-cache.php
@@ -2,7 +2,7 @@
 
 /*
 Plugin Name: W3 Total Cache
-Description: Dramatically improve the user experience of your blog. Add page caching, database caching, minify and content delivery network functionality and more to WordPress.
+Description: Dramatically improve the user experience of your blog. Add transparent page caching, database caching, minify and content delivery network functionality and more to WordPress.
 Version: 0.5
 Plugin URI: http://www.w3-edge.com/wordpress-plugins/w3-total-cache/
 Author: Frederick Townes
